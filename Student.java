2bd75650728ff1e7820ff11edb66c5302a4acbfc
Šß <xchen7640@bths.edu>
2017-11-21 17:40:30 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,27 @@
-
-public class Student {
-
-	public Student() {
-		// TODO Auto-generated constructor stub
+public class Student extends Person {
+	
+	private double gpa;
+	private int gradeLevel;
+	private String major;
+	
+	public Student (double gpa, int gradeLevel, String major) {
+		super (age, firstName,familyName,gender);
+		this.gpa = gpa;
+		this.gradeLevel = gradeLevel;
+		this.major = major;
 	}
-
+	public double getGpa () {
+		return gpa;
+	}
+	public int getGradeLevel () {
+		return gradeLevel;
+	}
+	public String getMajor() {
+		return major;
+	}
+	
+	public String toString() {
+		return getFamilyName()+ "," + getFirstName();
+	}
+	
 }
