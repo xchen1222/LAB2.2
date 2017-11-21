7dd2079f0db19740838710a2b1fba57d5e1f1804
Šß <xchen7640@bths.edu>
2017-11-21 17:40:50 -0500

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,8 +1,19 @@
-
-public class Teacher {
-
-	public Teacher() {
-		// TODO Auto-generated constructor stub
+public class Teacher extends Person {
+	
+	private String subject;
+	private String title;
+	
+	public Teacher (String subject, String title) {
+		super (age, firstName,familyName,gender);
+		this.subject = subject;
+		this.title = title;
 	}
 
+	public String getSubject() {
+		return subject;
+	}
+	public String toString() {
+		return title+ "," + getFamilyName();
+	}
+	
 }
