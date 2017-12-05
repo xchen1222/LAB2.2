7b488e07fcef6b14e91625a5abb0cf918510d200
User <User@User-PC.home>
2017-12-04 22:10:57 -0500

    I mean it kinda works?

--- Runner.java
+++ Runner.java
@@ -6,30 +6,37 @@ public class Runner {
 		Random rand = new Random();
 		int x = rand.nextInt(5);
 		int y = rand.nextInt(5);
-		int z = rand.nextInt(1);
+		int z = rand.nextInt(3);
 		int age = rand.nextInt(89)+10;
-		int gpa = rand.nextInt(50)+50;
+	//	int gpa = rand.nextInt(50)+50;
 		
 		String[] major =  {"Math " ,"Physics " , "Biology " , "Architecture " , "Economics " };
 		String[] gender = {"Male", "Female"};
 		String[] firstNames = {"a " , "B " , "c ", "d " , "e ", "f " };
 		String[] familyNames = {"d " , "e ", "f ", "a " , "B " , "c " };
 		String[] subject = {"math " , "english " , "??? ", "hadwja ", "wuajk " }; 
-		String[] title = {"mr." , "mrs."};
+		String[] title = {"mr." , "mrs." , "ms."};
 		
-	//	Person teacher = new Teacher(subject[x] , title[z] , age , firstNames[x], familyNames[y] , gender[z]);
-		Person student = new Student ( gpa, 12 , major[rand.nextInt(5)], age , firstNames[rand.nextInt(5)], familyNames[rand.nextInt(5)], gender[rand.nextInt(2)]);
+	//	Person teacher = new Teacher(subject[x] , title[z] , age , firstNames[x], familyNames[y] , gender[z]); 
+		
+		
 		System.out.print(title[z] );
 		System.out.print(firstNames[x] );
 		System.out.print(familyNames[y] );
 		System.out.println("teaches " + subject[x]);
-		
-		//Person[] students; 
+	//	int jn = 0 ;
 		for (int i = 0 ; i<34 ; i++) 
 		{
+		Person student = new Student ( rand.nextInt(50)+50, 12 , major[rand.nextInt(5)], age , firstNames[rand.nextInt(5)], familyNames[rand.nextInt(5)], gender[rand.nextInt(2)]);
+		
+		//jn = jn + rand.nextInt(50)+50 ;
 		System.out.println(student);
 		}
+		//System.out.println(jn); 
+		//Person[] students[n]; 
 		
+		
+	
 
 	}
 		
