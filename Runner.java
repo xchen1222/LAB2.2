f5cd5f8696ef70d2a16324b62b6227ed883986e8
User <User@User-PC.home>
2017-12-05 19:20:00 -0500

    Additional pylons were constructed

--- Runner.java
+++ Runner.java
@@ -13,18 +13,22 @@ public class Runner {
 		
 		String[] major =  {"Math " ,"Physics " , "Biology " , "Architecture " , "Economics " };
 		String[] gender = {"Male", "Female"};
-		String[] firstNames = {"a " , "B " , "c ", "d " , "e ", "f " };
-		String[] familyNames = {"d " , "e ", "f ", "a " , "B " , "c " };
-		String[] subject = {"math " , "english " , "??? ", "hadwja ", "wuajk " , "uwhjad" }; 
-		String[] title = {"mr." , "mrs." , "ms." , "mr."};
+		String[] firstNames = {"Adam " , "Ben " , "Colin ", "Daniel " , "Emily ", "Fermented ", "Wine ",
+				"Blair ", "Amy " , "Trust " , "Me " , "These ", "Names ", "Are ", "Totally ", "Legit "
+				, "Winry ", "Creep ", "Moon "};
+		String[] familyNames = {"Denice " , "Enmancipates ", "Froth ", "Appleseed " , "Beckons " , "Cooler "
+				, "Frezia " , "Last ", "Moon " ,"Additional " , "Pylons ", "Bench ", "The ", "Kench ", "Please " , 
+				"Let " , "MeOff ", "Thehook "};
+		String[] subject = {"Math " , "English " , "Philosophy ", "Social Studies ", "Computer Science " , "Engineering" }; 
+		String[] title = {"Mr." , "Mrs." , "Ms." , "Alien. "};
 		
 		Person teacher = new Teacher(subject[x] , title[z] , age , firstNames[x], familyNames[y] , gender[q]); 
 		
 		
-		System.out.print(title[z] );
-		System.out.print(firstNames[x] );
-		System.out.print(familyNames[y] );
-		System.out.print("teaches " + subject[x]);
+		System.out.print(title[rand.nextInt(4)] );
+		System.out.print(firstNames[rand.nextInt(19)] );
+		System.out.print(familyNames[rand.nextInt(18)] );
+		System.out.print("teaches " + subject[rand.nextInt(6)]);
 	//	int jn = 0 ;
 	/*	for (int i = 0 ; i<34 ; i++) 
 		{
@@ -38,7 +42,7 @@ public class Runner {
 		Person[] studentBody = new Person[34];
 		for (int i=0; i<studentBody.length;i++)	
 		{
-			studentBody[i]= new Student (  rand.nextDouble()+ rand.nextInt(50)+50, 12 , major[rand.nextInt(5)], age , firstNames[rand.nextInt(5)], familyNames[rand.nextInt(5)], gender[rand.nextInt(2)]) ;
+			studentBody[i]= new Student (  rand.nextDouble()+ rand.nextInt(50)+50, 12 , major[rand.nextInt(5)], age , firstNames[rand.nextInt(19)], familyNames[rand.nextInt(18)], gender[rand.nextInt(2)]) ;
 		}
 		Classroom classroom1 = new Classroom (studentBody , teacher) ; 
 		System.out.println(classroom1.printClass());
